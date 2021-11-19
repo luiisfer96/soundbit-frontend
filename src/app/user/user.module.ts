e10c17dbae6b201699460e4261b351dce277ead6
luiisfer96 <luiisfer96@hotmail.com>
2021-11-18 19:25:09 -0500

    primer commit paco

---
+++ src/app/user/user.module.ts
@@ -0,0 +1,20 @@
+import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+@NgModule ({
+    declarations: [
+        LoginComponent,
+        RegisterComponent
+    ],
+    exports: [
+        LoginComponent,
+        RegisterComponent
+    ],
+    imports: [
+        CommonModule
+    ]
+})
+
+export class UserModule {}
