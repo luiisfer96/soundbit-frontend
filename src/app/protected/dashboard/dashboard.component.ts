8e301fae07a267b51900540e191dc4d23bc8659c
Nicolás Rojas <rojasnicolas25.nr@gmail.com>
2021-12-02 22:08:06 -0500

    estilo corregido

--- src/app/protected/dashboard/dashboard.component.ts
+++ src/app/protected/dashboard/dashboard.component.ts
@@ -5,7 +5,9 @@ import { AuthService } from '../../auth/services/auth.service';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styles: []
+  styles: [
+    `* { margin: 15px }`
+  ]
 })
 export class DashboardComponent {
 
